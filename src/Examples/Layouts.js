69d96ba8dca0cc96e6869e600bf726392851ad39
ei.muginov <ei.muginov@tensor.ru>
2018-05-21 18:24:25 +0500

    123123123123

--- src/Examples/Layouts.js
+++ src/Examples/Layouts.js
@@ -50,8 +50,101 @@ export default class Layouts extends React.Component {
         </div>
         {this.dividerRender()}
         {this.layoutRender2()}
+         {this.renderComponents()}
+         {this.cardRender()}
+         {this.tabRender()}
       </div>
     );
+  }
+
+  /*modalRender() {
+     return <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+        <div class="modal-dialog" role="document">
+           <div class="modal-content">
+              <div class="modal-header">
+                 <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                 </button>
+              </div>
+              <div class="modal-body">
+                 ...
+              </div>
+              <div class="modal-footer">
+                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                 <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+           </div>
+        </div>
+     </div>;
+  }*/
+
+  renderComponents() {
+     return (<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">навигация</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                 <a class="nav-link" href="#">Домой <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Выпадающий
+                 </a>
+                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Действие1</a>
+                    <a class="dropdown-item" href="#">Действие2</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Чтотто</a>
+                 </div>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link disabled" href="#">залочено</a>
+              </li>
+           </ul>
+           <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
+           </form>
+        </div>
+     </nav>);
+  }
+
+  cardRender() {
+     return <div class="card">
+           <div class="card-body">
+              <h5 class="card-title">Заголовок</h5>
+              <p class="card-text">Текст для карт.</p>
+              <a href="#" class="btn btn-primary">Переходики</a>
+           </div>
+     </div>;
+  }
+
+  tabRender() {
+     return <ul class="nav nav-tabs">
+        <li class="nav-item">
+           <a class="nav-link active" href="#">Текущая</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="#">Ссылка1</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="#">Ссылка1</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link disabled" href="#">залоченная</a>
+        </li>
+     </ul>;
   }
 
   dividerRender() {
