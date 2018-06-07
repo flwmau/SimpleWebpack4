61a6dc793e662bc6a7b8379288c483aed018c545
ei.muginov <ei.muginov@tensor.ru>
2018-06-07 15:42:23 +0500

    Все тут

--- src/App/App.js
+++ src/App/App.js
@@ -1,35 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Progress } from 'reactstrap';
+import './App.css';
+
+
 
 export default class App extends React.Component {
+   constructor(props) {
+      super();
+      this.state = {
+         todos: []
+      }
+   }
+
+   render() {
+      return <div>
+         <input type="text" ref={(input) => { this.todoInput = input; }} />
+         <button onClick={this.addItem.bind(this)}>Добавить</button>
+         <button onClick={this.toggleAll.bind(this)}>Переключить все</button>
+         <ol>
+            {
+               this.state.todos.map(todo => {
+                  return <li key={todo.id} className={todo.checked ? 'checked' : ''}>
+                     {todo.text}
+                     <button onClick={this.toggleStatus.bind(this, todo)}>Сменить статус</button>
+                     <button onClick={this.removeTodo.bind(this, todo)}>Удалить</button>
+                  </li>
+               })
+            }
+         </ol>
+      </div>
+   }
+
+
+
+  /* rende1r() {
+      return <div>
+         Введите задачу тут:
+         <input type="text" ref={(input) => { this.todoInput = input; }} />
+         <button onClick={this.addItem.bind(this)}>Добавить</button>
+         <TodoList todos={this.state.todos} removeTodo={this.removeTodo.bind(this)}/>
+      </div>;
+   }*/
+
+   addItem() {
+      const cloneTodos = this.state.todos;
+      cloneTodos.push({id: Math.round(Math.random()*1000),text: this.todoInput.value, checked: false});
+      this.setState({
+         todos: cloneTodos
+      });
+   }
+
+   toggleStatus(toggleTodo) {
+      this.setState({
+         todos: this.state.todos.map(todo => {
+            if (todo.id === toggleTodo.id) {
+               todo.checked = !todo.checked;
+            }
+            return todo;
+         })
+      });
+   }
+
+   toggleAll() {
+      this.setState({
+         todos: this.state.todos.map(todo => {
+            todo.checked = !todo.checked;
+            return todo;
+         })
+      })
+   }
+
+   removeTodo(removeTodo) {
+      const newTodo = this.state.todos.filter(todo => todo.id !== removeTodo.id);
+      this.setState({
+         todos: newTodo
+      });
+   }
+}
+
+class TodoList extends React.Component {
    constructor(props) {
       super();
    }
 
    render() {
-      return <div> React Worked
-         <div>
-            <div className="text-center">0%</div>
-            <Progress />
-            <div className="text-center">25%</div>
-            <Progress value="25" />
-            <div className="text-center">50%</div>
-            <Progress value={50} />
-            <div className="text-center">75%</div>
-            <Progress value={75} />
-            <div className="text-center">100%</div>
-            <Progress value="100" />
-            <div className="text-center">Multiple bars</div>
-            <Progress multi>
-               <Progress bar value="15" />
-               <Progress bar color="success" value="30" />
-               <Progress bar color="info" value="25" />
-               <Progress bar color="warning" value="20" />
-               <Progress bar color="danger" value="5" />
-            </Progress>
-         </div>
-      </div>;
+      return <ol>
+         {this.props.todos.map(todo => {
+            return <Todo key={todo.id} item={ todo } removeTodo={this.props.removeTodo}/>
+         })}
+      </ol>
+   }
+}
+
+class Todo extends React.Component {
+   constructor(props) {
+      super();
+   }
+
+   render() {
+      return <li className="todo">
+         {this.props.item.text}
+         <button>Выполнено</button>
+         <button onClick={this.removeTodo.bind(this)}>Удалить</button>
+      </li>;
+   }
+
+   removeTodo(id) {
+      this.props.removeTodo(id);
    }
 }
 
